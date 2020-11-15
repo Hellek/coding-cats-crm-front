9f06052e42ca43ecdc47a8faaba2e8a43a0eebcc
Roman Basharin <r.basharin@itigris.ru>
2020-11-15 23:59:46 +0300

    Вёрстка хедера под мобайл

--- src/layout/LayoutHeader.vue
+++ src/layout/LayoutHeader.vue
@@ -1,14 +1,39 @@
 <template>
-	<header class="d-flex justify-content-between align-items-center py-1 px-5 shadow-hard flex-wrap">
-		<LayoutNav/>
+	<header class="d-flex justify-content-between align-items-center py-1 px-5 shadow-hard flex-wrap position-relative">
+		<i
+			v-if="$breakpoints.smAndDown"
+			class="el-icon-menu color-white font-size-large clickable"
+			@click="isDrawerVisible = true"
+		/>
+		<!-- @click="$refs.drawer.closeDrawer()" -->
+
+		<LayoutNav
+			v-if="!$breakpoints.smAndDown"
+		/>
+
+		<el-drawer
+			v-else
+			id="header-drawer"
+			ref="drawer"
+			:visible.sync="isDrawerVisible"
+			:with-header="false"
+			:append-to-body="true"
+			:show-close="false"
+			direction="ltr"
+			size="60%"
+		>
+			<LayoutNav
+				class="flex-column"
+				@clicked="isDrawerVisible = false"
+			/>
+		</el-drawer>
 
 		<div>
 			<!-- <ThemeSwitcher/> -->
 
 			<el-button
-				type="text"
-				class="ml-3"
-				@click="$store.dispatch('users/unauthorize')"
+				:loading="isUnauthorizing"
+				@click="unauthorize"
 			>Выход</el-button>
 		</div>
 	</header>
@@ -21,5 +46,26 @@ export default {
 		LayoutNav: () => import('./LayoutNav'),
 		// ThemeSwitcher: () => import('./ThemeSwitcher'),
 	},
+	data() {
+		return {
+			isUnauthorizing: false,
+			isDrawerVisible: false,
+		}
+	},
+	methods: {
+		async unauthorize() {
+			this.isUnauthorizing = true
+			await this.$store.dispatch('users/unauthorize')
+			this.isUnauthorizing = false
+		},
+	},
 }
 </script>
+
+<style lang="scss">
+#header-drawer {
+	.el-drawer__header {
+		display: none;
+	}
+}
+</style>
