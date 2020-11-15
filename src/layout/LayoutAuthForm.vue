3619bf6a1df708626fe9c20ded53107ef87539ec
Roman Basharin <r.basharin@itigris.ru>
2020-11-15 18:39:34 +0300

    Добавил логику сброса пароля

--- src/layout/LayoutAuthForm.vue
+++ src/layout/LayoutAuthForm.vue
@@ -4,18 +4,29 @@
 			v-model="form.email"
 			placeholder="Email"
 			class="mb-4"
+			:disabled="isChangePasswordMode"
 		/>
+
 		<ElInput
+			v-if="!isRememberPasswordMode"
 			v-model="form.password"
 			show-password
-			placeholder="Пароль"
-			class="mb-4"
+			:placeholder="isChangePasswordMode ? 'Новый пароль' : 'Пароль'"
+			:class="{'mb-4': !isForgetQuestionVisible}"
 		/>
+
+		<el-button
+			v-if="isForgetQuestionVisible && !isRememberPasswordMode"
+			type="text"
+			size="mini"
+			@click="isRememberPasswordMode = true"
+		>Я забыл пароль</el-button>
+
 		<ElButton
-			class="w-100p"
+			class="w-100p ml-0"
 			type="primary"
 			@click="signIn"
-		>Войти</ElButton>
+		>{{ signInLabel }}</ElButton>
 	</ElCard>
 </template>
 
@@ -24,17 +35,51 @@ export default {
 	name: 'LayoutAuthForm',
 	data() {
 		return {
+			isForgetQuestionVisible: false,
+			isChangePasswordMode: false,
+			isRememberPasswordMode: false,
 			form: {
 				email: '',
 				password: '',
 			},
 		}
 	},
+	computed: {
+		signInLabel() {
+			if (this.isRememberPasswordMode) return 'Отправить на почту'
+			return this.isChangePasswordMode ? 'Сохранить' : 'Войти'
+		},
+	},
+	created() {
+		if (this.$route.query['set-new-password'] === 'true') {
+			this.isChangePasswordMode = true
+
+			this.form = {
+				...this.form,
+				...this.$route.query,
+			}
+
+			this.$router.replace({ query: null })
+		}
+	},
 	methods: {
 		async signIn() {
 			try {
+				if (this.isRememberPasswordMode) {
+					const res = (await this.$http.post('auth/password/reset', this.form)).data
+					this.$notify.success({ title: res })
+					this.isForgetQuestionVisible = false
+					this.isRememberPasswordMode = false
+					return
+				}
+
+				if (this.isChangePasswordMode) {
+					await this.$http.put('auth/password', this.form)
+				}
+
 				await this.$store.dispatch('users/authorize', this.form)
 			} catch (error) {
+				if (!this.isChangePasswordMode) this.isForgetQuestionVisible = true
 				this.$notifyUserAboutError(error)
 			}
 		},
