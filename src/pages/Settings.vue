02efed4f480f88ca388ce51d4474bcc5dfe1a455
Roman Basharin <r.basharin@itigris.ru>
2020-12-19 23:17:02 +0300

    User integrated to Settings

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,11 +1,23 @@
 <template>
 	<div>
-		Settings
+		<User
+			:userId="user.id"
+		/>
 	</div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
 	name: 'Settings',
+	components: {
+		User: () => import('Components/User'),
+	},
+	computed: {
+		...mapState({
+			user: state => state.users.user,
+		}),
+	},
 }
 </script>
