7f3009f71f90e6414411d0faf723f99b59710d70
Roman Basharin <roman.basharin@quantori.com>
2021-06-26 03:43:39 +0300

    feat add time save at url

--- src/components/TinkoffInvest/DatesFromTo.vue
+++ src/components/TinkoffInvest/DatesFromTo.vue
@@ -11,6 +11,7 @@
 
 <script>
 import { mapState, mapMutations } from 'vuex'
+import { getUrlSearchParams, getTodaySessionTime } from 'Utils'
 
 export default {
 	name: 'DatesFromTo',
@@ -23,16 +24,40 @@ export default {
 			set(period) {
 				const [from, to] = period
 				this.setFilterTime({ from, to })
+				this.setQuery({ from, to })
 			},
 			get() {
 				return [this.filter.from, this.filter.to]
 			},
 		},
 	},
+	created() {
+		this.syncUrlAndStore()
+	},
 	methods: {
 		...mapMutations({
 			setFilterTime: 'tinkoffInvest/setFilterTime',
 		}),
+		setQuery({ from, to }) {
+			this.$router.replace({
+				query: {
+					...this.$route.query,
+					from: this.$dayjs(from).format(),
+					to: this.$dayjs(to).format(),
+				},
+			})
+		},
+		syncUrlAndStore() {
+			const query = getUrlSearchParams()
+			const defaultTime = getTodaySessionTime()
+			const isDefaultTime = defaultTime.from === this.filter.from && defaultTime.to === this.filter.to
+
+			if (query.from && isDefaultTime) {
+				this.setFilterTime({ from: query.from, to: query.to })
+			} else if (this.filter.from && !query.from) {
+				this.setQuery(this.filter)
+			}
+		},
 	},
 }
 </script>
